760fea236fa997287c959aaa0cf4ebcc0057d5a2
zy2003 <836579100@qq.com>
2022-07-26 20:33:37 +0800

    change

--- hello/src/Hello/Hello.java
+++ hello/src/Hello/Hello.java
@@ -1,7 +1,13 @@
 package Hello;
 
+import java.util.Scanner;
+
 public class Hello {
    public static void main(String[] args) {
         System.out.println("hello world!");
+        /*Scanner in = new Scanner(System.in);
+        System.out.println("echo:" + in.nextLine());
+        in.close();*/
+        System.out.println("2 + 3 = " + (2 + 3));
    }
 }
